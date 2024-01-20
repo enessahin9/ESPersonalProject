085b7244ee92b4b12b8ff641fdb7979bcb47bf79
Enes Şahin <enessahin9@outlook.com>
2024-01-20 20:46:30 +0300

    Default Controller added

---
+++ ESPersonalProject/Controllers/DefaultController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ESPersonalProject.Controllers
+{
+	public class DefaultController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+
+		}
+
+		public PartialViewResult HeadPartial()
+		{
+			return PartialView();
+		}
+
+		public PartialViewResult HeaderPartial()
+		{
+			return PartialView();
+
+		}
+
+		public PartialViewResult HeroSectionPartial()
+		{
+			return PartialView();
+		}
+
+		public PartialViewResult AboutPartial()
+		{
+			return PartialView();
+		}
+	}
+}
