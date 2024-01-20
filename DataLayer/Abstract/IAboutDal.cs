fa2d82af339500aa202c422445f731e062521f0f
Enes Şahin <enessahin9@outlook.com>
2024-01-20 21:13:17 +0300

    Interface created

---
+++ DataLayer/Abstract/IAboutDal.cs
@@ -0,0 +1,15 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// About icin ekstra bir ozellik tanimlanacaksa IGeneric icerisine degil, IAboutDal.cs icerisine yazilacak.
+
+namespace DataLayer.Abstract
+{
+	public interface IAboutDal : IGenericDal<About>
+	{
+	}
+}
---
+++ DataLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Ortak kullanılacak oldugum crud islemleri icin generic interface olusturuldu.
+
+namespace DataLayer.Abstract
+{
+	public interface IGenericDal<T> where T : class
+	{
+		void Add(T t);
+		void Delete(T t);
+		void Update(T t);
+		List<T> GetList();
+		T GetById(int id);
+	}
+}
