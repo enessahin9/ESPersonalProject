528ac0a379841ac4da8499670d58caf32f40a929
Enes Şahin <enessahin9@outlook.com>
2024-01-20 21:22:27 +0300

    Repository Pattern added

---
+++ DataLayer/Repository/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using DataLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository
+{
+	public class GenericRepository<T> : IGenericDal<T> where T : class
+	{
+		private readonly AppDbContext _appDbContext;
+
+		public GenericRepository(AppDbContext appDbContext)
+		{
+			_appDbContext = appDbContext;
+		}
+
+		public void Add(T t)
+		{
+			_appDbContext.Add(t);
+			_appDbContext.SaveChanges();
+		}
+
+		public void Delete(T t)
+		{
+			_appDbContext.Remove(t);
+			_appDbContext.SaveChanges();
+		}
+
+		public T GetById(int id)
+		{
+			return _appDbContext.Set<T>().Find(id);
+		}
+
+		public List<T> GetList()
+		{
+			return _appDbContext.Set<T>().ToList();
+		}
+
+		public void Update(T t)
+		{
+			_appDbContext.Update(t);
+			_appDbContext.SaveChanges();
+		}
+	}
+}
