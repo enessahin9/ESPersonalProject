6192e0afe44caf65536363e0fe11cddfde5722c1
Enes Şahin <enessahin9@outlook.com>
2024-01-20 20:10:19 +0300

    DataBase connection added

--- ESPersonalProject/Program.cs
+++ ESPersonalProject/Program.cs
@@ -1,9 +1,17 @@
+using DataLayer;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("constr")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
