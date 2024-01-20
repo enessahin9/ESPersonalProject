b967bc1a441491c113997ab28cd507811e5a16fd
Enes Şahin <enessahin9@outlook.com>
2024-01-20 21:56:48 +0300

    Manager added

---
+++ Bussines/Content/AboutManager.cs
@@ -0,0 +1,47 @@
+﻿using Bussines.Service;
+using DataLayer.Abstract;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussines.Content
+{
+	public class AboutManager : IGenericService<About>, IAboutService
+	{
+
+		private readonly IAboutDal _aboutDal;
+
+		public AboutManager(IAboutDal aboutDal)
+		{
+			_aboutDal = aboutDal;
+		}
+
+		public void TAdd(About t)
+		{
+			_aboutDal.Add(t);
+		}
+
+		public void TDelete(About t)
+		{
+			_aboutDal.Delete(t);
+		}
+
+		public About TGetById(int id)
+		{
+			return _aboutDal.GetById(id);
+		}
+
+		public List<About> TGetList()
+		{
+			return _aboutDal.GetList();
+		}
+
+		public void TUpdate(About t)
+		{
+			_aboutDal.Update(t);
+		}
+	}
+}
