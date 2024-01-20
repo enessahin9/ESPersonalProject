73b11874ae3d6a8483e1b3c9adcd1667432746a3
Enes Şahin <enessahin9@outlook.com>
2024-01-20 19:47:29 +0300

    Add project files.

---
+++ Model/BaseModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class BaseModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsDeleted { get; set; } = false;
+        public bool IsActive { get; set; } = true;
+        public DateTime DataCreated { get; set; } = DateTime.Now;
+        public DateTime? DateModified { get; set; }
+    }
+}
---
+++ Model/Work.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Work : BaseModel
+    {
+        public string Image { get; set; }
+
+        public string? Title { get; set; }
+
+    }
+}
