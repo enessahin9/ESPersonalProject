3f036879febb60d9a0ef6f45fdf16bbc8315b689
Enes Şahin <enessahin9@outlook.com>
2024-02-07 14:57:54 +0300

    error message controller

---
+++ ESPersonalProject/Areas/Admin/Controllers/MessageController.cs
@@ -0,0 +1,54 @@
+﻿using Business.Service;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using System.Drawing;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ESPersonalProject.Areas.Admin.Controllers;
+
+[Area("Admin")]
+public class MessageController : Controller
+{
+	private readonly IMessageService _messageService;
+
+	public MessageController(IMessageService messageService)
+	{
+		_messageService = messageService;
+	}
+
+	public IActionResult Index()
+	{
+		return View();
+	}
+	public IActionResult GetAll()
+	{
+		return Json(_messageService.TGetList());
+	}
+
+	[HttpGet]
+	public IActionResult MessageAdd()
+	{
+		return View();
+	}
+
+	[HttpPost]
+	public IActionResult MessageAdd(Message message)
+	{
+		_messageService.TAdd(message);
+		return Ok();
+	}
+
+	public IActionResult MessageUpdate()
+	{
+		return View();
+	}
+
+
+	public IActionResult MessageDelete(int id)
+	{
+		var values = _messageService.TGetById(id);
+		_messageService.TDelete(values);
+		return Ok();
+	}
+}
+
