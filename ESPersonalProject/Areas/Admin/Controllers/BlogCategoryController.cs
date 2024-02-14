dc8d6167fb075d4f0f9f271bbd62edf0e61c70c7
Enes Şahin <enessahin9@outlook.com>
2024-02-14 12:58:52 +0300

    Theme changed

---
+++ ESPersonalProject/Areas/Admin/Controllers/BlogCategoryController.cs
@@ -0,0 +1,46 @@
+﻿using Business.Service;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace ESPersonalProject.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class BlogCategoryController : Controller
+	{
+		private readonly IBlogCategoryService _blogCategoryService;
+
+		public BlogCategoryController(IBlogCategoryService blogCategoryService)
+		{
+			_blogCategoryService = blogCategoryService;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult GetAll()
+		{
+			var values = _blogCategoryService.TGetList();
+			return Json(values);
+		}
+		[HttpGet]
+		public IActionResult AddBlog()
+		{
+			return View();
+		}
+		[HttpPost]
+		public IActionResult AddBlog(BlogCategory blogCategory)
+		{
+		  _blogCategoryService.TAdd(blogCategory);
+			return Ok();
+		}
+		[HttpPost]
+		public IActionResult DeleteBC(int id)
+		{
+			var values = _blogCategoryService.TGetById(id);
+			_blogCategoryService.TDelete(values);
+			return Ok();
+		}
+	}
+}
+
---
+++ ESPersonalProject/Areas/Admin/Controllers/BlogDetailController.cs
@@ -0,0 +1,53 @@
+﻿using Business.Service;
+using Helper;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+
+namespace ESPersonalProject.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class BlogDetailController : Controller
+	{
+		private readonly IBlogDetailService _blogDetailService;
+        private readonly FileUpload _fileUpload;
+
+        public BlogDetailController(IBlogDetailService blogDetailService ,FileUpload fileUpload)
+		{
+			_blogDetailService = blogDetailService;
+			_fileUpload = fileUpload;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult GetALl()
+		{
+			
+			return View(_blogDetailService.TGetList());
+		}
+		[HttpGet]
+		public IActionResult AddBlogDetail()
+		{
+			return View();
+		}
+        [HttpPost]
+        public IActionResult AddBlogDetail(BlogDetail blogDetail, IFormFile ImageUrl, IFormFile ImageUrl2, IFormFile ImageUrl3, IFormFile ImageUrl4)
+        {
+
+            var uploadedImageUrl = FileUpload.UploadFile(ImageUrl);
+            var uploadedImageUrl2 = FileUpload.UploadFile(ImageUrl2);
+            var uploadedImageUrl3 = FileUpload.UploadFile(ImageUrl3);
+            var uploadedImageUrl4 = FileUpload.UploadFile(ImageUrl4);
+            blogDetail.ImageUrl = uploadedImageUrl;
+            blogDetail.ImageUrl2 = uploadedImageUrl2;
+            blogDetail.ImageUrl3 = uploadedImageUrl3;
+           
+
+
+            _blogDetailService.TAdd(blogDetail);
+
+            return RedirectToAction("Index");
+        }
+    }
+}
