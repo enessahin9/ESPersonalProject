116191db62852cdc61b2db6c7f9798470c73f734
Enes Şahin <enessahin9@outlook.com>
2024-02-14 12:56:44 +0300

    Blog managers added

---
+++ Bussines/Content/BlogCategoryManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Service;
+using DataLayers.Abstract;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Content;
+
+public class BlogCategoryManager : IGenericService<BlogCategory>, IBlogCategoryService
+{
+	private readonly IBlogCategoryDal _blogCategoryDal;
+
+	public BlogCategoryManager(IBlogCategoryDal blogCategoryDal)
+	{
+		_blogCategoryDal = blogCategoryDal;
+	}
+
+	public void TAdd(BlogCategory t)
+	{
+		_blogCategoryDal.Add(t);
+	}
+
+	public void TDelete(BlogCategory t)
+	{
+		_blogCategoryDal.Delete(t);
+	}
+
+	public BlogCategory TGetById(int id)
+	{
+		return _blogCategoryDal.GetById(id);
+	}
+
+	public List<BlogCategory> TGetList()
+	{
+		return _blogCategoryDal.GetList();
+	}
+
+	public void TUpdate(BlogCategory t)
+	{
+		_blogCategoryDal.Update(t);
+	}
+}
---
+++ Bussines/Content/BlogDetailManager.cs
@@ -0,0 +1,45 @@
+﻿using Business.Service;
+using DataLayers.Abstract;
+using Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Content;
+
+public class BlogDetailManager : IGenericService<BlogDetail>, IBlogDetailService
+{
+	private readonly IBlogDetailDal _blogDetailDal;
+
+	public BlogDetailManager(IBlogDetailDal blogDetailDal)
+	{
+		_blogDetailDal = blogDetailDal;
+	}
+
+	public void TAdd(BlogDetail t)
+	{
+		_blogDetailDal.Add(t);
+	}
+
+	public void TDelete(BlogDetail t)
+	{
+		_blogDetailDal.Delete(t);
+	}
+
+	public BlogDetail TGetById(int id)
+	{
+		return _blogDetailDal.GetById(id);
+	}
+
+	public List<BlogDetail> TGetList()
+	{
+		return _blogDetailDal.GetList();
+	}
+
+	public void TUpdate(BlogDetail t)
+	{
+		_blogDetailDal.Update(t);
+	}
+}
