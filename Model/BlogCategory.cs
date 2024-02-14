6474dea5a82fb46440cf51e9d8ec18f7b71845aa
Enes Şahin <enessahin9@outlook.com>
2024-02-14 12:59:29 +0300

    Blog models added

---
+++ Model/BlogCategory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models;
+
+public class BlogCategory:BaseModel
+{
+    public ICollection<BlogDetail> BlogDetails { get; set; }
+}
---
+++ Model/BlogDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class BlogDetail:BaseModel
+	{
+		public string Description { get; set; }
+        public string? Description2 { get; set; }
+		public string? Description3 { get; set;}
+        public string ImageUrl { get; set; }
+        public string? ImageUrl2 { get; set; }
+
+		public string? ImageUrl3 { get; set;}
+        public string? Tags { get; set; }
+		public ICollection<BlogCategory> BlogCategories { get; set; }
+	}
+}
