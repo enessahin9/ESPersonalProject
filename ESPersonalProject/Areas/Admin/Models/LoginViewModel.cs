c03104b31c623b6b7301932de73d1f95fa806146
Enes Şahin <enessahin9@outlook.com>
2024-02-16 17:16:02 +0300

    Login and Register added

---
+++ ESPersonalProject/Areas/Admin/Models/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ESPersonalProject.Areas.Admin.Models
+{
+    public class LoginViewModel
+    {
+        
+            public string UserName { get; set; }
+
+            public string Password { get; set; }
+
+        
+    }
+}
