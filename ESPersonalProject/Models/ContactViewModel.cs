44c2aac45c855bb6f417d887dc1614af48a74ac7
Enes Şahin <enessahin9@outlook.com>
2024-04-01 02:59:59 +0300

    ContactViewModel added

---
+++ ESPersonalProject/Models/ContactViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Models;
+
+namespace ESPersonalProject.Models;
+
+public class ContactViewModel
+{
+	public string Name { get; set; }
+	public string Email { get; set; }
+	public string Subject { get; set; }
+	public string Body { get; set; }
+}
